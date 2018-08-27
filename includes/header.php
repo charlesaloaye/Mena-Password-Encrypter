d5b5258dfc86a9d1334ae2e11aad5e6cf87ee2f4
Sedenu Aloaye Charles <aloayecharles@gmail.com>
2018-08-27 11:48:50 -0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
     <meta name="keywords" content="<?php echo APP_KEYWORDS;?>">
 
   <!--Cascading Style Sheet (CSS)-->
-  	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+  	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
